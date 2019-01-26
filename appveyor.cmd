be5a8fc5aa66d41ffc5ddf39426f0732563ee548
Rob Mensching <rob@firegiant.com>
2019-01-25 17:16:33 -0800

    Error when appveyor.cmd fails

--- appveyor.cmd
+++ appveyor.cmd
@@ -1,17 +1,18 @@
 @setlocal
 @pushd %~dp0
 
-nuget restore
+nuget restore || set _ERR=1
 
-msbuild -p:Configuration=Release;Platform=x86;PlatformToolset=v140_xp
-msbuild -p:Configuration=Release;Platform=x64;PlatformToolset=v140_xp
+msbuild -p:Configuration=Release;Platform=x86;PlatformToolset=v140_xp || set _ERR=1
+msbuild -p:Configuration=Release;Platform=x64;PlatformToolset=v140_xp || set _ERR=1
 
-msbuild -p:Configuration=Release;Platform=x86;PlatformToolset=v141_xp
-msbuild -p:Configuration=Release;Platform=x64;PlatformToolset=v141_xp
-msbuild -p:Configuration=Release;Platform=ARM;PlatformToolset=v141
-msbuild -p:Configuration=Release;Platform=ARM64;PlatformToolset=v141
+msbuild -p:Configuration=Release;Platform=x86;PlatformToolset=v141_xp || set _ERR=1
+msbuild -p:Configuration=Release;Platform=x64;PlatformToolset=v141_xp || set _ERR=1
+msbuild -p:Configuration=Release;Platform=ARM;PlatformToolset=v141 || set _ERR=1
+msbuild -p:Configuration=Release;Platform=ARM64;PlatformToolset=v141 || set _ERR=1
 
-msbuild -p:Configuration=Release -t:PackNativeNuget src\dutil\dutil.vcxproj
+msbuild -p:Configuration=Release -t:PackNativeNuget src\dutil\dutil.vcxproj || set _ERR=1
 
 @popd
-@endlocal
+@if "%_ERR%"=="1" (echo. & echo Build FAILED. 1>&2)
+@exit /b %_ERR%
